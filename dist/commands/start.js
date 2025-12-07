27a0e18c27ee447dc0ffea2530802e4931cc8739
PaulGraur <115890528+PaulGraur@users.noreply.github.com>
2025-12-07 15:57:43 +0100

    add changes

--- dist/commands/start.js
+++ dist/commands/start.js
@@ -3,27 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.startCommand = startCommand;
 const grammy_1 = require("grammy");
 function startCommand(bot) {
-    bot.command("start", async (ctx) => {
-        await showMainMenu(ctx);
-    });
-    bot.callbackQuery("mainMenu", async (ctx) => {
-        await showMainMenu(ctx);
-    });
+  bot.command("start", async (ctx) => {
+    await showMainMenu(ctx);
+  });
+  bot.callbackQuery("mainMenu", async (ctx) => {
+    await showMainMenu(ctx);
+  });
 }
 async function showMainMenu(ctx) {
-    const keyboard = new grammy_1.InlineKeyboard()
-        .text("➕ Додати слово", "add")
-        .row()
-        .text("🔁 Повторити слова", "repeat")
-        .row()
-        .text("✏️ Тренування написання", "train")
-        .row()
-        .text("📚 Список слів", "listwords");
-    if (ctx.callbackQuery) {
-        await ctx.editMessageText("Обери дію:", { reply_markup: keyboard });
-        await ctx.answerCallbackQuery();
-    }
-    else {
-        await ctx.reply("Обери дію:", { reply_markup: keyboard });
-    }
+  const keyboard = new grammy_1.InlineKeyboard()
+    .text("➕ Додати слово", "add")
+    .row()
+    .text("🔁 Повторити слова", "repeat")
+    .row()
+    .text("📚 Список слів", "listwords");
+  if (ctx.callbackQuery) {
+    await ctx.editMessageText("Обери дію:", { reply_markup: keyboard });
+    await ctx.answerCallbackQuery();
+  } else {
+    await ctx.reply("Обери дію:", { reply_markup: keyboard });
+  }
 }
