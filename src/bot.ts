e988156bd0df7c5603b70350bfe6386c3d95761f
PaulGraur <115890528+PaulGraur@users.noreply.github.com>
2025-12-07 15:11:49 +0100

    add changes

--- src/bot.ts
+++ src/bot.ts
@@ -7,11 +7,11 @@ import { repeatWordsCommand } from "./commands/repeatWords.js";
 import { listWordsCommand } from "./commands/listWords.js";
 
 const token = process.env.BOT_TOKEN;
-if (!token)
+if (!token) {
   throw new Error("❌ BOT_TOKEN не встановлено у Environment Variables");
+}
 
 export const bot = new Bot<BotContext>(token);
-
 bot.use(session({ initial: () => ({}) }));
 
 startCommand(bot);
@@ -23,4 +23,17 @@ bot.start({
   onStart: (info) => {
     console.log(`Бот запущено! Username: @${info.username}`);
   },
+});
+
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`HTTP server running on port ${PORT}`);
 });
